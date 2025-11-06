b131992b16a5a34a15f7c8d1ba23ad4d89604827
bojanzavojchevski <bojannn890@gmail.com>
2025-11-06 06:22:16 +0100

    .

---
+++ BarberBookingApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberBookingApp.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
